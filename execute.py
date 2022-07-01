e7eb72709fae93f6b055d203a22213333c886b38
Liontea9048 <hsb9048@snu.ac.kr>
2022-07-01 14:14:28 +0900

    udpate

--- execute.py
+++ execute.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
 
         modifyExample(numCores, nps)
         modifyAmbeSource(pos)
-        modifyTETDetector(pos)
+        modifyTETDetector(pos, posture=posture)
 
         try:
             makedirs(outputPath)
