1e8b6ecfc12e882efc2741dd81a225c5049a38b6
Liontea9048 <hsb9048@snu.ac.kr>
2022-09-11 14:57:55 +0900

    yVarianceAnalyze

--- base_dist_and_flux/src/TETRunAction.cc
+++ base_dist_and_flux/src/TETRunAction.cc
@@ -74,8 +74,9 @@ void TETRunAction::EndOfRunAction(const G4Run* aRun)
 	PrintResult(ofs);
 	ofs.close();
 
-	G4cout<< outputFile_flux.c_str() << G4endl;
+	
 	outputFile_flux = outputFile - ".out" + "_flux.out";
+	G4cout<< outputFile_flux.c_str() << G4endl;
 	std::ofstream ofs_flux(outputFile_flux.c_str());
 	PrintResult_flux(ofs_flux);
 	ofs_flux.close();
